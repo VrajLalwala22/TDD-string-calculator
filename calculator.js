f59f9b883ceb7ab6832f608afcb26f736db22ffa
VrajLalwala22 <vrajlalwala22@gmail.com>
2025-07-05 17:30:52 +0530

    feat: support sum of two numbers separated by comma

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
 function add(numbers) {
   if (numbers === "") return 0;
   // For a single number string, return its integer value
-  return parseInt(numbers, 10);
+  if (!numbers.includes(",")) return parseInt(numbers, 10);
+  const [a, b] = numbers.split(",");
+  return parseInt(a, 10) + parseInt(b, 10);
 }
 
 module.exports = add; 
