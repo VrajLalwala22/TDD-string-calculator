3bd19c855160f770c0f07ca74b946d35991784cb
VrajLalwala22 <vrajlalwala22@gmail.com>
2025-07-05 17:44:09 +0530

    test: add failing test to ignore numbers bigger than 1000

--- calculator.test.js
+++ calculator.test.js
@@ -26,4 +26,8 @@ test('returns sum when using custom delimiter', () => {
 
 test('throws exception for negative numbers with message including all negatives', () => {
   expect(() => add("1,-2,3,-4")).toThrow("negatives not allowed: -2,-4");
+});
+
+test('ignores numbers bigger than 1000', () => {
+  expect(add("2,1001")).toBe(2);
 }); 
