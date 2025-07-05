5d01a045bb96ad00939822d261817ed49ff2ecb2
VrajLalwala22 <vrajlalwala22@gmail.com>
2025-07-05 17:23:01 +0530

    test: add failing test for single number input

--- calculator.test.js
+++ calculator.test.js
@@ -2,4 +2,8 @@ const add = require('./calculator');
 
 test('returns 0 for empty string', () => {
   expect(add("")).toBe(0);
+});
+
+test('returns number for single number string', () => {
+  expect(add("5")).toBe(5);
 }); 
