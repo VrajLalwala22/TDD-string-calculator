1c36e156e6b44f031362d405e443dcba9064e734
VrajLalwala22 <vrajlalwala22@gmail.com>
2025-07-05 17:40:27 +0530

    feat: support custom delimiters in add function

--- calculator.js
+++ calculator.js
@@ -1,8 +1,14 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  return numbers
-    .replace(/\n/g, ",")
-    .split(",")
+  let delimiter = /,|\n/;
+  let nums = numbers;
+  if (numbers.startsWith("//")) {
+    const delimiterLineEnd = numbers.indexOf("\n");
+    delimiter = new RegExp(numbers[2].replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+    nums = numbers.slice(delimiterLineEnd + 1);
+  }
+  return nums
+    .split(delimiter)
     .map(n => parseInt(n, 10))
     .reduce((sum, n) => sum + n, 0);
 }
