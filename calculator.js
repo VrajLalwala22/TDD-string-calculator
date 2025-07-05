c6deeb646716129b985f8a0a768dbc34785567a6
VrajLalwala22 <vrajlalwala22@gmail.com>
2025-07-05 17:37:06 +0530

    feat: support new lines as delimiters in add function

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 function add(numbers) {
   if (numbers === "") return 0;
   return numbers
+    .replace(/\n/g, ",")
     .split(",")
     .map(n => parseInt(n, 10))
     .reduce((sum, n) => sum + n, 0);
